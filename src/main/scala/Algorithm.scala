cbfe744590ed0210c6ddfa2695f8ef7eb756ce02
ukitaka <yuki.takahashi.1126@gmail.com>
2018-03-19 22:46:13 +0900

    Add algorithm w

---
+++ src/main/scala/Algorithm.scala
@@ -0,0 +1,7 @@
+object AlgorithmU {
+  def mostGeneralUnifier(type1: TypeExpression, type2: TypeExpression): Set[Substitution] = ???
+}
+
+object AlgorithmW {
+  def apply(assignments: Set[TypeAssignment], term: Term): (Set[Predicate], Set[TypeAssignment], TypeExpression) = ???
+}
--- src/main/scala/AlgorithmU.scala
+++
@@ -1,3 +0,0 @@
-object AlgorithmU {
-  def mostGeneralUnifier(type1: TypeExpression, type2: TypeExpression): Set[Substitution] = ???
-}
